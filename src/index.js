1c85c729c9240dbbe61dc817ae52b124cd6a9527
Ivan <ivan@sq10.net>
2016-10-13 16:11:32 -0500

    current code and stuff
    dumping it onto github

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Event system service.
+// This service is responsible for:
+// - Having a server that takes events
+// - ???
+
+Promise.resolve().then(async () => {
+
+}).catch((err) => {
+  console.log('Service failed')
+  console.error((err && err.stack) ? err.stack : err)
+})
